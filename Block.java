14c8a0ea7d66a57f127c2b25f5bd83c3ef20836d
Monkey77771 <152912874+Monkey77771@users.noreply.github.com>
2023-12-07 18:34:03 +0000

    added Block.java

---
+++ Block.java
@@ -0,0 +1,35 @@
+import java.awt.Graphics;
+import java.awt.Image;
+import java.net.URL;
+import javax.imageio.ImageIO;
+
+
+public class Block {
+    private int x;
+    private int y;
+    private int s; // s by s block
+    private int h; // # hits to die
+    private Image img;
+    
+    public Block(int x, int y){
+        this.x = x;
+        this.y = y;
+        s = 20;
+        try{
+            URL url = getClass().getResource("BrownBlock.jpeg");
+            img = ImageIO.read(url);
+        } catch(Exception e){}
+    }
+    public int getX(){ return x; }
+    public int getY(){ return y; }
+    public int getS(){ return s; }
+    public int getHealth(){ return h; }
+    public void setX(int x){ this.x = x; }
+    public void setY(int y){ this.y = y; }
+    public void changeX(int dx){ x += dx; }
+    public void changeY(int dy){ y += dy; }
+    public void decHealth(){ h--; }
+    public void draw(Graphics window){
+        window.drawImage(img, getX(), getY(), getS(), getS(),null);
+    }
+}
