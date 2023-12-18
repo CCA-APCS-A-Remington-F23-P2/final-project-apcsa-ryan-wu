1378dca45f2c74c08477ee375ac9b13196f1637b
JenR01 <149633670+JenR01@users.noreply.github.com>
2023-12-18 18:10:14 +0000

    idk what i changed

---
+++ PowerUps.java
@@ -0,0 +1,31 @@
+import java.util.*;
+public class PowerUps extends Player{
+  ArrayList<String> playerpowers = new ArrayList<String>();
+  
+  public void addSpeed(Player player){
+    player.setXSpeed(XSpeed+5);
+    player.setYSpeed(YSpeed+5);
+  }
+  public void AOE(Player player){
+    for(int i=player.getX()-50; i<=player.getX()+50; i++){
+      for(int j=player.getY-50; j<=player.getY+50; j++){
+        //if collide with something subtract life
+      }
+    }
+  }
+  public void heal(Player player){
+    //add health to player, prevent damage for short duration
+  }
+  public void piercingShots(Player player){
+    //bullets are not destroyed after collisions
+  }
+  public void wall(Player player){
+    //build metal wall in front of player
+  }
+  public void conjureEvilMan(Player player){
+    //create an evil man that follows the other player
+  }
+  public void shrink(Player player){
+    //shrink player dimensions
+  }
+}
