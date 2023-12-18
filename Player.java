99da381222cf1180882523f6dc859b5c2c164f6c
KoushikJandhyala <105557310+KoushikJandhyala@users.noreply.github.com>
2023-12-18 18:18:17 +0000

    Added default constructor

--- Player.java
+++ Player.java
@@ -14,17 +14,21 @@ public class Player extends MovingThing {
   private int count = 0;
   private long lastJump = 0;
 
+  public Player() {
+    this(10, 10, 10, 10, 1, 1, 1);
+  }
+
   public Player(int player) {
     this(10, 10, 10, 10, 1, 1, player);
   }
 
   public Player(int x, int y, int player) {
-    //add code here
+    // add code here
     this(x, y, 10, 10, 1, 1, player);
   }
 
   public Player(int x, int y, int ys, int xs, int player) {
-    //add code here
+    // add code here
     this(x, y, 10, 10, ys, xs, player);
 
   }
@@ -39,13 +43,14 @@ public class Player extends MovingThing {
         URL url = getClass().getResource("Square.png");
         image = ImageIO.read(url);
       } catch (Exception e) {
-        //feel free to do something here
+        // feel free to do something here
       }
     } else {
       try {
         URL url = getClass().getResource("RedSquare.png");
         image = ImageIO.read(url);
-      } catch (Exception e) {}
+      } catch (Exception e) {
+      }
     }
   }
 
@@ -66,32 +71,31 @@ public class Player extends MovingThing {
   }
 
   public void move(List<Block> blocks) {
-      if(getYSpeed()==0 && getY() != 319 && getY() != 499) System.out.println(getY());
-      double dx = getXSpeed()/10.0;
-      double dy = getYSpeed()/10.0;
-      double x = getX();
-      double y = getY();
-      boolean xDone = false;
-      boolean yDone = false;
-      for(int i = 0; i < 10; i++){
-            x += dx;
-            y -= dy;
-            if(!xDone){
-                setX((int) Math.round(x));
-                if(isTouching(blocks)){
-                    setX((int) Math.round(x-dx));
-                    xDone = true;
-                }
-            }
-            if(!yDone){
-                setY((int) Math.round(y));
-                  if(isTouching(blocks)){
-                      setY((int) Math.round(y+dy));
-                      yDone = true;
-                  }
-            }
-
+    double dx = getXSpeed() / 10.0;
+    double dy = getYSpeed() / 10.0;
+    double x = getX();
+    double y = getY();
+    boolean xDone = false;
+    boolean yDone = false;
+    for (int i = 0; i < 10; i++) {
+      x += dx;
+      y -= dy;
+      if (!xDone) {
+        setX((int) Math.round(x));
+        if (isTouching(blocks)) {
+          setX((int) Math.round(x - dx));
+          xDone = true;
+        }
       }
+      if (!yDone) {
+        setY((int) Math.round(y));
+        if (isTouching(blocks)) {
+          setY((int) Math.round(y + dy));
+          yDone = true;
+        }
+      }
+
+    }
   }
 
   public void gravity() {
@@ -110,33 +114,40 @@ public class Player extends MovingThing {
   }
 
   private boolean segmentsOverlap(int s1, int e1, int s2, int e2) {
-    if (s1 < s2) return e1 >= s2;
-    else return e2 >= s1;
+    if (s1 < s2)
+      return e1 >= s2;
+    else
+      return e2 >= s1;
   }
 
-    public boolean isTouching(Block b){
-        return segmentsOverlap(getX(), getX()+getWidth(), b.getX(), b.getX()+b.getS())
-            && segmentsOverlap(getY(), getY()+getHeight(), b.getY(), b.getY()+b.getS());
-    }
+  public boolean isTouching(Block b) {
+    return segmentsOverlap(getX(), getX() + getWidth(), b.getX(), b.getX() + b.getS())
+        && segmentsOverlap(getY(), getY() + getHeight(), b.getY(), b.getY() + b.getS());
+  }
 
-    public boolean isTouching(List<Block> blocks){
-        for(Block b : blocks) if(isTouching(b)) return true; return false;
-    }
+  public boolean isTouching(List<Block> blocks) {
+    for (Block b : blocks)
+      if (isTouching(b))
+        return true;
+    return false;
+  }
 
-    public boolean blockAbove(List<Block> blocks){
-        boolean ret = false;
-        setY(getY()-1);
-        if(isTouching(blocks)) ret = true;
-        setY(getY()+1);
-        return ret;
-    }
+  public boolean blockAbove(List<Block> blocks) {
+    boolean ret = false;
+    setY(getY() - 1);
+    if (isTouching(blocks))
+      ret = true;
+    setY(getY() + 1);
+    return ret;
+  }
 
-    public boolean blockBelow(List<Block> blocks){
-        boolean ret = false;
-        setY(getY()+1);
-        if(isTouching(blocks)) ret = true;
-        setY(getY()-1);
-        return ret;
-    }
+  public boolean blockBelow(List<Block> blocks) {
+    boolean ret = false;
+    setY(getY() + 1);
+    if (isTouching(blocks))
+      ret = true;
+    setY(getY() - 1);
+    return ret;
+  }
 
 }
